aa5b4a3d23e22ea2bfaf7b39c4e47b788b93116f
Pranta07 <palpranta@gmail.com>
2021-08-28 03:43:39 +0600

    user-details

--- js/random-users.js
+++ js/random-users.js
@@ -1,5 +1,5 @@
 const loadData = () => {
-    fetch("https://randomuser.me/api/?results=5")
+    fetch("https://randomuser.me/api/")
         .then((res) => res.json())
         .then((data) => displayUsers(data.results));
 };
@@ -9,8 +9,14 @@ const displayUsers = (users) => {
     // console.log(users);
     const userDiv = document.getElementById("users");
     users.forEach((user) => {
-        const p = document.createElement("p");
-        p.innerText = `Email: ${user.email}`;
-        userDiv.appendChild(p);
+        // const p = document.createElement("p");
+        // p.innerText = `Email: ${user.email}`;
+        // userDiv.appendChild(p);
+        userDiv.innerHTML = `
+            <img src="${user.picture.large}" alt="">
+            <p>Email: ${user.email}</p>
+            <p>Name: ${user.name.title} ${user.name.first} ${user.name.last}</p>
+            <p>Location: ${user.location.street.name}, ${user.location.street.number}, ${user.location.city}, ${user.location.country}</p>
+        `;
     });
 };
