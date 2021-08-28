7ff3757a2a2e743eb3d7749e97e01535e38f5852
Pranta07 <palpranta@gmail.com>
2021-08-28 12:01:37 +0600

    multiple-random-users added

--- js/random-users.js
+++ js/random-users.js
@@ -1,5 +1,5 @@
 const loadData = () => {
-    fetch("https://randomuser.me/api/")
+    fetch("https://randomuser.me/api/?results=20")
         .then((res) => res.json())
         .then((data) => displayUsers(data.results));
 };
@@ -12,11 +12,16 @@ const displayUsers = (users) => {
         // const p = document.createElement("p");
         // p.innerText = `Email: ${user.email}`;
         // userDiv.appendChild(p);
-        userDiv.innerHTML = `
-            <img src="${user.picture.large}" alt="">
+        const div = document.createElement("p");
+        div.style.border = "1px solid black";
+        div.style.borderRadius = "10px";
+        div.style.padding = "10px";
+        div.innerHTML = `
+            <img src="${user.picture.large}" alt="" width="100%">
             <p>Email: ${user.email}</p>
             <p>Name: ${user.name.title} ${user.name.first} ${user.name.last}</p>
             <p>Location: ${user.location.street.name}, ${user.location.street.number}, ${user.location.city}, ${user.location.country}</p>
         `;
+        userDiv.appendChild(div);
     });
 };
